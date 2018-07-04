9a27845132755226d2d555eb5bf2fc8f7a1e6927
Oleg Sukhov <osukhov21@gmail.com>
2018-07-04 20:03:07 +0300

    CAC-0000: Working with forms: assignments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,19 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer: string;
   genders = ['male', 'female'];
-  user = { username: '', email: '', secretQuestion: '', answer: '', gender: ''};
+  user = { username: '', email: '', secretQuestion: '', answer: '', gender: '' };
   submited = false;
+
+  @ViewChild('fr') mailForm: NgForm;
+  levels = ['basic', 'advanced', 'pro'];
+  defaultLevel = 'advanced';
+  mailSubmited = false;
+  mailData = { email: '', password: '', level: '' };
 
   // onSubmit(form: NgForm) {
   //   console.log(form);
   // }
+
   onSubmit() {
     this.user.username = this.signupForm.value.userData.username;
     this.user.email = this.signupForm.value.userData.email;
@@ -26,6 +33,15 @@ export class AppComponent {
     this.user.gender = this.signupForm.value.gender;
     this.submited = true;
     this.signupForm.reset();
+  }
+
+  onMailSubmit() {
+    this.mailData.email = this.mailForm.value.mail;
+    this.mailData.password = this.mailForm.value.password;
+    this.mailData.level = this.mailForm.value.level;
+    this.mailSubmited = true;
+    console.log(this.mailForm.value);
+    this.mailForm.reset();
   }
 
   fillUserName() {
