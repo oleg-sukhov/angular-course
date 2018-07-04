1a4d17618eebef2f467ba057a9560457b498768c
Oleg Sukhov <osukhov21@gmail.com>
2018-07-04 15:04:28 +0300

    CAC-0000: Working with forms: template approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'pet';
+  answer: string;
+  genders = ['male', 'female'];
+  user = { username: '', email: '', secretQuestion: '', answer: '', gender: ''};
+  submited = false;
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+  onSubmit() {
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.answer;
+    this.user.gender = this.signupForm.value.gender;
+    this.submited = true;
+    this.signupForm.reset();
+  }
+
+  fillUserName() {
+    this.signupForm.form.patchValue({
+      userData: {
+        username: 'TestUser'
+      }
+    });
+  }
 }
