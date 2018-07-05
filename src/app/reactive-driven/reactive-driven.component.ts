6b783a09f853edb770afe3cfeb07b87a85662fde
Oleg Sukhov <osukhov21@gmail.com>
2018-07-05 10:04:20 +0300

    CAC-0000: Working with reactive forms approach

--- src/app/reactive-driven/reactive-driven.component.ts
+++ src/app/reactive-driven/reactive-driven.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { resolve } from 'dns';
 
 @Component({
   selector: 'app-reactive-driven',
@@ -7,9 +10,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveDrivenComponent implements OnInit {
 
-  constructor() { }
+  genders = ['male', 'female'];
+  forbiddenNames = ['Anna', 'Mark'];
+  signupForm: FormGroup;
 
   ngOnInit() {
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl(null, [Validators.required, this.isNameForbidden.bind(this)]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.isEmailForbidden)
+      }),
+      'gender': new FormControl(this.genders[0]),
+      'hobbies': new FormArray([])
+    });
+    this.signupForm.statusChanges.subscribe((status) => console.log(status));
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'DefaultUserName'
+      }
+    });
   }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  isNameForbidden(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenNames.indexOf(control.value) !== -1) {
+      return { isNameForbidden: true };
+    }
+    return null;
+  }
+
+  isEmailForbidden(control: FormControl): Promise<any> | Observable<any> {
+    return new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 't@email.ua') {
+          return resolve({ emailForbidden: true });
+        } else {
+          return resolve(null);
+        }
+      }, 2000);
+    });
+  }
+
 
 }
